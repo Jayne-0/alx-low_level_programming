664c3fb501a95c26b52f4f2d8cd3e250cde69fc3
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-07 13:13:41 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * print_alphabet_x10 - print alphabet
 *
@@ -6,10 +7,10 @@
 */
 void print_alphabet_x10(void)
 {
-  int a, b;
-  for (a = 0; a <= 9; a++)
+int a, b;
+for (a = 0; a <= 9; a++)
 {
-  for (b = 97; b <= 122; b++)
+for (b = 97; b <= 122; b++)
 {
 _putchar(b);
 }
