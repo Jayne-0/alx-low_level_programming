f722b2cae4bdb7ec318710bec2f51e90a6ec70ef
Jayne-0 <105009097+Jayne-0@users.noreply.github.com>
2022-06-10 02:50:02 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
-* print_most_numbers - print 0 to 9, skip 2 and 4
+* print_most_numbers - prints numbers between 0 to 9
+* (neither 2 nor 4).
+* Return: no return.
 */
 void print_most_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
+int ch;
+for (ch = 48; ch < 58; ch++)
 {
-if (i != '2' && i != '4')
-_putchar(i);      
+if (ch != 50 && ch != 52)
+_putchar(ch);
 }
 _putchar('\n');
 }
