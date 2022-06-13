71af6eb8471ec309e2fb8511d8c41aff3f799527
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-13 11:04:26 -0700

    Strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+*File : 2-strlen.c
+*Auth : Jayne-0 .
+*/
+
+#include "main.h"
+/*
+*_strlen() - return length of a string .
+*/
+
+int _strlen(char *s)
+{
+int c = 0;
+while (*s != '\0')
+{
+c++;
+s++;
+}
+return (c);
+}
