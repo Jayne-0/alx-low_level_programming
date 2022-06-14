427fd575ef5fab0ca9cd46ea3c3b5e3e63cfb4a8
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-14 05:43:33 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strlen() - calculates the length of a string.
+* _strlen - calculates the length of a string.
 * @s: pointer to the character array.
 * Return: length of the string
 */
