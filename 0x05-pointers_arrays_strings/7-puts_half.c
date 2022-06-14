843f9627fa896f9925a920f550c3207df731ef6a
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-14 08:05:28 -0700

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,8 @@ length++;
 if (length % 2 == 0)
 n = length / 2;
 else
-n = (length +1) / 2;
+n = (length + 1) / 2;
 for (i = n; i < length; i++)
-  _putchar(str[i]);
+_putchar(str[i]);
 _putchar('\n');
 }
