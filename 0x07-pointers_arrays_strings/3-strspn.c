40ad29eb98a042526c1152cd696bd4b00b65f3ac
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-20 17:53:16 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,10 +2,11 @@
 
 /**
 * _strspn - gets the length of a prefix substring
-* @s: segment to compare bytes from
+* @s: segment to compare bytes from  .
 * @accept: string of bytes to compare with
 * Return: number of bytes in segment s which consist of bytes from accept
 */
+
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
@@ -13,7 +14,7 @@ i = 0;
 while (s[i] != '\0')
 {
 j = 0;
-while (accept[j] != '\0' && s[i] != accept [j])
+while (accept[j] != '\0' && s[i] != accept[j])
 j++;
 if (accept[j] == '\0')
 return (i);
