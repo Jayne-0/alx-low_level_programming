4cd4499e6057acf10c7e70cd13db24375d4bf013
Jayne-0 <Affamuefuna.jane@yandex.com>
2022-06-22 02:07:41 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 * _pow_recursion - returns the value of x raised to the power of y.
 * @x: base.
 * @y: exponent.
-* return: value of the exponentiation. if y is lower than 0, return -1.
+* Return: value of the exponentiation. if y is lower than 0, return -1.
 */
 int _pow_recursion(int x, int y)
 {
